aa5e28e0ccc73597d714661fb5f6bc101541f2ab
EvsikovaN <evsikova32@gmail.com>
2022-09-22 14:31:56 +0300

    fix: after rewiew

--- app.js
+++ app.js
@@ -5,17 +5,9 @@ const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cors = require('cors');
 const { limiter } = require('./utils/limitter');
-
-const { validateSignUp, validateAuth } = require('./utils/validation');
-const { auth } = require('./middlewares/auth');
+const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-const { userRoutes } = require('./routes/userRoutes');
-const { movieRoutes } = require('./routes/movieRoutes');
-const { createUser, login } = require('./controllers/userControllers');
-
 const { handleError } = require('./middlewares/handleError');
-const { NotFoundError } = require('./errors/not-found-err');
 
 const { PORT = 3500, DATABASE = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
 
@@ -23,33 +15,10 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-
 app.use(express.json());
-
 app.use(requestLogger);
 app.use(limiter);
-
-app.post(
-  '/signin',
-  validateAuth,
-  login,
-);
-
-app.post(
-  '/signup',
-  validateSignUp,
-  createUser,
-);
-
-app.use(auth);
-
-app.use('/users', userRoutes);
-app.use('/movies', movieRoutes);
-
-app.use((req, res, next) => {
-  next(new NotFoundError('Запрашиваемой страницы не существует'));
-});
-
+app.use(router);
 app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
 app.use(handleError);
